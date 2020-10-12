514ef7586e6ef1f4dde18c1c35fdd3bbfabc2417
Patrick Beltran <66067267+pgbeltran15@users.noreply.github.com>
2020-10-12 09:55:59 -0400

    intial

---
+++ static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoicGdiZWx0cmFuMTUiLCJhIjoiY2tmdHBtaGZtMHI5ejJwbnUwc2U4Y2g4YiJ9.fKfitqAzKhbv4uRblHBkOA";
---
+++ static/js/logic.js
@@ -0,0 +1,50 @@
+var base_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+function createMap() {
+
+    var streetMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/streets-v11",
+        accessToken: API_KEY
+    });
+
+    var lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "light-v10",
+        accessToken: API_KEY
+    });
+
+
+    // Define Variables for Tile Layers
+    var satelliteMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "mapbox.satellite",
+        accessToken: API_KEY
+    });
+
+
+    var baseLayers = {
+        "Street": streetMap,
+        "Light": lightMap,
+        "Satellite": satelliteMap
+    };
+
+    var overlays = {
+        "Earthquakes": earthquakes,
+    };
+
+    var myMap = L.map('mapid', {
+        center: [37.09, -95.71],
+        zoom: 5,
+    
+        layers: [streetMap, earthquakes]
+    });
+
+    L.control.layers(baseLayers, overlays).addTo(myMap);
+    
+}
